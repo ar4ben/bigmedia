a2105836a2072bd944253bc901bb541f6c75f615
ar4ben <arthur.rodzkin@gmail.com>
2017-05-14 04:42:42 +0300

    add categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+  has_many :categorization
+  has_many :categories, through: :categorization
+
+  validate :has_category
+
+  def has_category
+    errors.add(:base, 'Должна быть выбрана категория') if self.categorization.blank?
+  end
 end
