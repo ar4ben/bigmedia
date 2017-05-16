d41ffa0383d79be50034e8b279deff4a5255f071
ar4ben <arthur.rodzkin@gmail.com>
2017-05-16 04:22:27 +0300

    add slugs to articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,21 @@
 class Article < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
   acts_as_taggable
+
   has_many :categorization
   has_many :categories, through: :categorization
 
   validate :has_category
+  validates :title, :slug, presence: true
+
+  private
 
   def has_category
     errors.add(:base, 'Должна быть выбрана категория') if self.categorization.blank?
+  end
+
+  def should_generate_new_friendly_id?
+    slug.nil? || title_changed? 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170514215555) do
+ActiveRecord::Schema.define(version: 20170516011732) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "articles", ["slug"], name: "index_articles_on_slug", unique: true
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true
 
   create_table "categorizations", force: :cascade do |t|
     t.integer  "article_id"
