87fb9bc0779cff2eabd11199cf83cb3271dc42f3
ar4ben <arthur.rodzkin@gmail.com>
2017-09-02 05:57:44 +0300

    add styles for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,17 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_most_popular, only: [:index, :show]
   # GET /articles
   # GET /articles.json
   def index
 
     @articles = Article.order('created_at desc').paginate(:page => params[:page], 
                                                           :per_page => 18)
-    @most_popular = Article.where(
-      'published = :t and published_at >= :seven_days_ago', 
-      { t: true, seven_days_ago: Time.now - 7.days }
-    ).order('views desc').limit 6
   end
 
   # GET /articles/1
   # GET /articles/1.json
   def show
-    @most_popular = []
     @article.update( 
       views: @article.views + 1
     )
@@ -27,5 +22,12 @@ class ArticlesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_article
     @article = Article.friendly.find(params[:id])
+  end
+
+  def set_most_popular
+    @most_popular = Article.where(
+      'published = :t and published_at >= :seven_days_ago', 
+      { t: true, seven_days_ago: Time.now - 7.days }
+    ).order('views desc').limit 6
   end
 end
