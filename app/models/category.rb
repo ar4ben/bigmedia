024ab0cf6f45ce591922ef5a0541e00aab727731
ar4ben <arthur.rodzkin@gmail.com>
2017-10-07 15:41:03 +0300

    add russian slug for rubric and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,10 @@ class Category < ActiveRecord::Base
   has_many :categorization
   has_many :articles, through: :categorization
 
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
   private
 
   def should_generate_new_friendly_id?
--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -4,6 +4,10 @@ class Rubric < ActiveRecord::Base
 
   has_many :articles
 
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
   private
 
   def should_generate_new_friendly_id?
